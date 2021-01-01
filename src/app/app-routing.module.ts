da5f6750908f2f6c1662fd6e48f2123e90f2465d
Shahbaz135 <shahbazmuhammad135@gmail.com>
2021-01-01 11:59:48 +0500

    message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'en',
+  //   pathMatch: 'full',
+  // },
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./pages/static/static.module').then((m) => m.StaticModule),
+      },
+      {
+        path: 'account',
+        loadChildren: () =>
+          import('./pages/account/account.module').then((m) => m.AccountModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
