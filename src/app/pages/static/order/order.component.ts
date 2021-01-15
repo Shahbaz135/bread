25ad9f2228fbd731f12a82086012fcda9ab57ab6
Shahbaz135 <shahbazmuhammad135@gmail.com>
2021-01-15 17:02:08 +0500

    add/remove product

--- src/app/pages/static/order/order.component.ts
+++ src/app/pages/static/order/order.component.ts
@@ -7,13 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
   public active = 1;
-  
+
+  public product1Value = 0;
+  public product2Value = 0;
 
   constructor() { }
-  
 
   ngOnInit(): void {
-    
+  }
+
+  toggleMore(value): void {
+    if ( value == 1) {
+      this.product1Value++;
+    }
+
+    if ( value == 2) {
+      this.product2Value++;
+    }
+  }
+
+  toggleLess(value): void {
+    if (value == 1) {
+      if (this.product1Value > 0) {
+        this.product1Value--;
+      }
+    }
+
+    if (value == 2) {
+      if (this.product2Value > 0) {
+        this.product2Value--;
+      }
+    }
   }
 
 }
