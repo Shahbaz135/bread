8ca6adbf70f9210db29e44c8777b18519fa1a5d1
waseem abbas <mianwaseemabbas@gmail.com>
2021-02-03 19:40:21 +0500

    merge

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public LogInForm: FormGroup;
+
+  public static matchValues(
+    matchTo: string // name of the control to match to
+  ): (AbstractControl) => ValidationErrors | null {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return !!control.parent &&
+        !!control.parent.value &&
+        control.value === control.parent.controls[matchTo].value
+        ? null
+        : { isMatching: 'no' };
+    };
+ }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private toasterService: ToastrService,
+  ) { }
 
   ngOnInit(): void {
+    this.LogInForm = this.formBuilder.group({ 
+      phone: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
+     });
   }
+
+  get rf() {
+    return this.LogInForm.controls;
+  }
+  
+  
+
+  
 
 }
