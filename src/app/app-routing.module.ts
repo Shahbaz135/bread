3a9e09ed504d0c8936ac3f8ca737d0dcf4b8f6a2
Shahbaz135 <shahbazmuhammad135@gmail.com>
2021-03-28 11:36:43 +0500

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountLayoutComponent } from './layouts/account-layout/account-layout.component';
 import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { AuthGuard } from './services/common/auth.guard';
 
 const routes: Routes = [
   // {
@@ -20,6 +21,7 @@ const routes: Routes = [
       },
       {
         path: 'account',
+        canActivate: [AuthGuard],
         component: AccountLayoutComponent,
         loadChildren: () =>
           import('./pages/account/account.module').then((m) => m.AccountModule),
